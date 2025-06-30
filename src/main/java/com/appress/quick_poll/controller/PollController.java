6aac115a3400d26f3019da3c7934e0f387b269d5
Fisnik Shabani <fisnik.shabani+backbase@endava.com>
2025-06-30 23:37:47 +0200

    Set Location header in createPoll endpoint to return resource URI

--- src/main/java/com/appress/quick_poll/controller/PollController.java
+++ src/main/java/com/appress/quick_poll/controller/PollController.java
@@ -2,14 +2,18 @@ package com.appress.quick_poll.controller;
 
 import javax.inject.Inject;
 
+import java.net.URI;
+
 import com.appress.quick_poll.domain.Poll;
 import com.appress.quick_poll.repository.PollRepository;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class PollController {
@@ -26,6 +30,17 @@ public class PollController {
     @PostMapping("/polls")
     public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
         poll = pollRepository.save(poll);
-        return new ResponseEntity<>(null, HttpStatus.CREATED);
+
+        //Set the Location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPollUri = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(poll.getId())
+            .toUri();
+
+        responseHeaders.setLocation(newPollUri);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
 }
