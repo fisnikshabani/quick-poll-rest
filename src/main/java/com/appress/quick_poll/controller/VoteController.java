c5dddeefe4799c1e5e8f1228b2a563bb6fb32222
Fisnik Shabani <fisnik.shabani+backbase@endava.com>
2025-07-01 23:30:05 +0200

    Add GET endpoint to retrieve all votes for a poll

--- src/main/java/com/appress/quick_poll/controller/VoteController.java
+++ src/main/java/com/appress/quick_poll/controller/VoteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,5 +33,10 @@ public class VoteController {
                 .toUri());
 
         return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/polls/{pollId}/votes")
+    public Iterable<Vote> getAllVotes(@PathVariable Long pollId) {
+        return voteRepository.findVotesByPoll(pollId);
     }
 }
--- src/main/java/com/appress/quick_poll/repository/VoteRepository.java
+++ src/main/java/com/appress/quick_poll/repository/VoteRepository.java
@@ -1,7 +1,12 @@
 package com.appress.quick_poll.repository;
 
 import com.appress.quick_poll.domain.Vote;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VoteRepository extends CrudRepository<Vote, Long> {
+
+    @Query(value = "select v.* from Option o, Vote v where o.POLL_ID = ?1 and " +
+        "v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
+    public Iterable<Vote> findVotesByPoll(Long pollId);
 }
