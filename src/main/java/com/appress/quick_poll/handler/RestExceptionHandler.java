1cc1a49b18065a40016c36cb9ac6cc7098c6040e
Fisnik Shabani <fisnik.shabani@gmail.com>
2025-07-07 16:41:30 +0200

    Enhance error handling by adding ResourceNotFoundException support and custom messages for validation errors

--- src/main/java/com/appress/quick_poll/handler/RestExceptionHandler.java
+++ src/main/java/com/appress/quick_poll/handler/RestExceptionHandler.java
@@ -2,51 +2,82 @@ package com.appress.quick_poll.handler;
 
 import com.appress.quick_poll.dto.error.ErrorDetails;
 import com.appress.quick_poll.dto.error.ValidationError;
+import com.appress.quick_poll.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleResourceNotFoundException(MethodArgumentNotValidException mavne, HttpServletRequest request ) {
+    @Autowired
+    private MessageSource messageSource;
 
-        ErrorDetails errorDetails = new ErrorDetails();
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
 
-        errorDetails.setTimestamp(new Date().getTime());
-        errorDetails.setStatus(HttpStatus.BAD_REQUEST.value());
-        String requestPath = (String) request.getAttribute("javax.servlet.error.request_uri");
-        if (requestPath == null) {
-            requestPath = request.getRequestURI();
-        }
-        errorDetails.setTitle("Validation Failed");
-        errorDetails.setDetail("Input Validation Failed");
-        errorDetails.setDeveloperMessage(mavne.getClass().getName());
+        ErrorDetails errorDetail = new ErrorDetails();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle("Resource Not Found");
+        errorDetail.setDetail(rnfe.getMessage());
+        errorDetail.setDeveloperMessage(rnfe.getClass().getName());
 
-        //Create ValidationError instances
-        List<FieldError> fieldErrors = mavne.getBindingResult().getFieldErrors();
-        for (FieldError fieldError : fieldErrors) {
-            List<ValidationError> validationErrorList = errorDetails.getErrors().get(fieldError.getField());
-            if (validationErrorList == null) {
-                validationErrorList = new ArrayList<ValidationError>();
-                errorDetails.getErrors().put(fieldError.getField(), validationErrorList);
-            }
-            ValidationError validationError = new ValidationError();
-            validationError.setCode(fieldError.getCode());
-            validationError.setMessage(fieldError.getDefaultMessage());
-            validationErrorList.add(validationError);
-        }
-
-        return new ResponseEntity<>(errorDetails, null, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manve, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setTimestamp(new Date().getTime());
+        errorDetails.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetails.setTitle("Validation Failed");
+        errorDetails.setDetail("Input Validation Failed");
+        errorDetails.setDeveloperMessage(manve.getClass().getName());
+
+        // Create ValidationError instances
+        List<FieldError> fieldErrors =  manve.getBindingResult().getFieldErrors();
+        for(FieldError fe : fieldErrors) {
+
+            List<ValidationError> validationErrorList = errorDetails.getErrors().get(fe.getField());
+            if(validationErrorList == null) {
+                validationErrorList = new ArrayList<ValidationError>();
+                errorDetails.getErrors().put(fe.getField(), validationErrorList);
+            }
+            ValidationError validationError = new ValidationError();
+            validationError.setCode(fe.getCode());
+            validationError.setMessage(messageSource.getMessage(fe, null));
+            validationErrorList.add(validationError);
+        }
+        return handleExceptionInternal(manve, errorDetails, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setTimestamp(new Date().getTime());
+        errorDetails.setStatus(status.value());
+        errorDetails.setTitle("Message Not Readable");
+        errorDetails.setDetail(ex.getMessage());
+        errorDetails.setDeveloperMessage(ex.getClass().getName());
+
+        return handleExceptionInternal(ex, errorDetails, headers, status, request);
+    }
 }
