419d21a40fe2de28b7362d56ba9147327069366c
Fisnik Shabani <fisnik.shabani+backbase@endava.com>
2025-07-15 16:15:52 +0200

    Add QuickPollClientV3 with basic authentication support
    - Implement DELETE operation with Basic Auth using `QuickPollClientV3BasicAuth`.

---
+++ src/main/java/com/appress/quick_poll/client/QuickPollClientV3BasicAuth.java
@@ -0,0 +1,38 @@
+package com.appress.quick_poll.client;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+public class QuickPollClientV3BasicAuth {
+
+    public static final String QUICK_POLL_URI_v3 = "http://localhost:8080/v3/polls";
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public void deletePoll(Long pollId) {
+        HttpHeaders authenticationHeaders = getAuthenticationHeader("admin", "admin");
+        restTemplate.exchange(QUICK_POLL_URI_v3 + "/{pollId}", HttpMethod.DELETE, new HttpEntity<Void>(authenticationHeaders), Void.class, pollId);
+    }
+
+    private HttpHeaders getAuthenticationHeader(String username, String password) {
+
+        String credentials = username + ":" + password;
+        byte[] base64CredentialData = Base64.getEncoder().encode(credentials.getBytes(StandardCharsets.UTF_8));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Basic " + new String(base64CredentialData, StandardCharsets.UTF_8));
+
+        return headers;
+    }
+
+    public static void main(String[] args) {
+
+        QuickPollClientV3BasicAuth client = new QuickPollClientV3BasicAuth();
+        client.deletePoll(1L);
+    }
+}
